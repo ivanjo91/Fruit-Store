623c6d1ec5ff50089447a8025d2f8506b8c9275b
ivanjo91 <50482763+ivanjo91@users.noreply.github.com>
2020-02-11 21:17:40 +0100

    backup

---
+++ TPV_Frutas/TPV_Frutas/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace TPV_Frutas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Location = new System.Drawing.Point(671, 11);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(75, 23);
+            this.btnAdmin.TabIndex = 1;
+            this.btnAdmin.Text = "Administrar";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(581, 534);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 55);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo Cliente";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(590, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 411);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(473, 230);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Producto";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Precio Unitario";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Peso";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Valor Parcial";
+            this.Column4.Name = "Column4";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Enabled = false;
+            this.btnModificar.Location = new System.Drawing.Point(671, 85);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Visible = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(671, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(644, 373);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(671, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Contraseña:";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 651);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnAdmin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TPV_Frutas/TPV_Frutas/Form1.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPV_Frutas
+{
+    public partial class Form1 : Form
+    {
+
+        String[] nombres = { "chirimoya", "ciruela", "fresa", "kiwi", "mandarina", "melocoton", "melon", "naranja", "nectarina", "papaya", "peras", "piña", "peras", "piña", "platanos", "pomelos", "prunus", "sandias" };
+        String[] precios = { "1,25", "2,75", "3,25", "1,50", "2,75", "3,25", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3" };
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            for(int i=0; i<nombres.Length; i++)
+            {
+
+                TableLayoutPanel panel = new TableLayoutPanel();
+                panel.ColumnCount = 1;
+                panel.RowCount = 2;
+                panel.AutoSize = true;
+
+                
+                Button botonX = new Button();
+                botonX.Tag = i;
+                botonX.Size = new Size(80, 80);
+                botonX.BackgroundImage = Image.FromFile(@"imagenes\" + nombres[i] + ".PNG");
+                botonX.BackgroundImageLayout = ImageLayout.Zoom;
+
+                botonX.Click += new EventHandler(clickFruta);
+                
+                
+                panel.Controls.Add(botonX);
+                
+
+                TextBox textbox = new TextBox();
+                textbox.Tag = i;
+                textbox.Width = 80;
+                textbox.Text = precios[i];
+                textbox.Enabled = false;
+
+                panel.Controls.Add(textbox);
+
+                tableLayoutPanel1.Controls.Add(panel);
+               
+            }
+
+            calcularTotal();
+                        
+        }
+
+        private void clickFruta(object sender, EventArgs e)
+        {
+            Button botonX = (Button)sender;
+            
+            String cadena = Interaction.InputBox("Introduce cantidad", "TPV Fruta");
+            //Reemplazar punto por coma
+            cadena = cadena.Replace('.', ',');           
+
+            try
+            {
+                float cantidad = float.Parse(cadena);
+                float precio = float.Parse(precios[Convert.ToInt32(botonX.Tag)]);
+                float totalParcial = cantidad * precio;
+
+                //Añadir fila
+                dataGridView1.Rows.Add();
+                //Obtener posición de la fila
+                int indiceFila = dataGridView1.RowCount - 1;
+                //Añadir nombre de fruta
+                dataGridView1.Rows[indiceFila].Cells[0].Value = nombres[Convert.ToInt32(botonX.Tag)];
+                //Añadir precio
+                dataGridView1.Rows[indiceFila].Cells[1].Value = precios[Convert.ToInt32(botonX.Tag)];
+                //Añadir cantidad
+                dataGridView1.Rows[indiceFila].Cells[2].Value = cantidad;
+                //Añadir total
+                dataGridView1.Rows[indiceFila].Cells[3].Value = totalParcial;
+
+                calcularTotal();
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Cantidad introducida no válida");
+            }
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("frutas"))
+            {
+                foreach(Control ctrl in tableLayoutPanel1.Controls)
+                {
+                    foreach(Control ctrl2 in ctrl.Controls)
+                    {
+                        ctrl2.Enabled = true;
+                    }
+                }
+                btnModificar.Enabled = true;
+            }
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox1.Enabled = true;
+            btnModificar.Visible = true;
+            textBox1.Focus();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            calcularTotal();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in tableLayoutPanel1.Controls)
+            {
+                foreach (Control ctrl2 in ctrl.Controls)
+                {
+                    if(ctrl2 is TextBox)
+                    {
+                        precios[Convert.ToInt32(ctrl2.Tag)] = ctrl2.Text;
+                        ctrl2.Enabled = false;
+                    }
+                }
+            }
+
+            label2.Visible = false;
+            textBox1.Text = "";
+            textBox1.Visible = false;
+            textBox1.Enabled = false;
+            btnModificar.Visible = false;
+            btnModificar.Enabled = false;
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+            calcularTotal();
+        }
+
+        private void calcularTotal()
+        {
+            float total = 0;
+
+            for(int i=0; i<dataGridView1.RowCount; i++)
+            {
+                total += float.Parse(dataGridView1.Rows[i].Cells[3].Value.ToString());
+            }
+
+            label1.Text = total + "€";
+        }
+    }
+}
